aabfd0eed68400700cb5e86811eec468e3702500
Ulysse Lebigre <ulysselebigre@gmail.com>
2026-06-21 07:55:58 +0200

    New_band

--- app/components/Marquee.tsx
+++ app/components/Marquee.tsx
@@ -7,8 +7,8 @@ function MarqueeContent() {
       {Array.from({ length: 8 }).map((_, i) => (
         <span key={i} className="flex items-center gap-8 px-8 whitespace-nowrap">
           <span className="text-4xl font-bold text-brown">New Plugin Vocobox</span>
-          <Image src="/LuLu_Plug_Text.png" alt="LuluPlug" width={120} height={48} className="object-contain" />
-          <Image src="/vocobox_logo.png" alt="Vocobox" width={48} height={48} className="object-contain" />
+          <Image src="/vocobox_logo.png" alt="Vocobox" width={68} height={68} className="object-contain" />
+          <Image src="/LuLu_Plug_Text.png" alt="LuluPlug" width={120} height={48} className="object-contain relative left-2" />
         </span>
       ))}
     </>
@@ -20,7 +20,7 @@ export default function Marquee() {
     <div className="overflow-hidden border-y border-border bg-dim py-6">
       <div
         className="flex w-max"
-        style={{ animation: "marquee 40s linear infinite" }}
+        style={{ animation: "marquee 70s linear infinite" }}
       >
         <div className="flex">
           <MarqueeContent />
